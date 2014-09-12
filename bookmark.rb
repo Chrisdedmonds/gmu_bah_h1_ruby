e0d931e1c68d20adc5061c840c0a9f6f418edb11
Chris Edmonds <chris.edmonds128@gmail.com>
2014-09-12 04:26:18 -0400

    Assignment directions

---
+++ bookmark.rb
@@ -0,0 +1,6 @@
+# Create a class named Bookmark with:
+# An initializer that accepts url and title parameters:
+# it should set instance variables named url, title
+# it should set a read only variable named last_visited
+# a method named visit! which sets last_visited to the current time and date 
+# hint: the Time.now methods returns an instance of Time
