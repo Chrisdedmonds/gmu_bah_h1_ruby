f7ba9bcc6808c86764a568051f10921062af77d7
chrisdedmonds <chris.edmonds128@gmail.com>
2014-10-07 21:57:32 -0400

    7

--- bookmark.rb
+++ bookmark.rb
@@ -4,3 +4,14 @@
 # it should set a read only variable named last_visited
 # a method named visit! which sets last_visited to the current time and date 
 # hint: the Time.now methods returns an instance of Time
+class Bookmark
+  def initialize(url, title)
+    @url = url
+    @title = title
+  end
+  
+  def visit!
+    last_visited = Time.now  
+  end
+end
+
